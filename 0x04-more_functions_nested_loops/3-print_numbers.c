6ab312656b87f1629f3e73cd3893d9969a40bd24
Alex Kinyua <alexkinyua001alx@gmail.com>
2023-03-24 05:42:36 -0700

    prints numbers 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * Return: Null
+ */
+
+void print_numbers(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+		_putchar(ch);
+	}
+
+	_putchar('\n');
+
+	return (0);
+
+}
