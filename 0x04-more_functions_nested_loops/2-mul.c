52424dc3385c963bc0b70350c544e79b531ca4ce
Alex Kinyua <alexkinyua001alx@gmail.com>
2023-03-24 05:30:37 -0700

    function print the multiple of 2 numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * mul - multiplies the numbers passed on the parameter
+ *
+ * @a: first parameter to be passed
+ *
+ * @b: second parameter to be passed
+ *
+ * Return: return the multiple/ result
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+
+}
