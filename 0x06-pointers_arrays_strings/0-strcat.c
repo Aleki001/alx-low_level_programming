bdd18d403be0ebfcb6bfcd6a58f5a99a50b24d4b
Alex Kinyua <alexkinyua001alx@gmail.com>
2023-03-29 22:50:59 -0700

    Joining 2 strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcat - concats 2 strings
+ * @dest: destination string
+ *@src: source string
+ *Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	for (int j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+
+}
+
