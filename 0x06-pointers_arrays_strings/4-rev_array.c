2f5dfe7e3f083d0f2236d297e734f882e0afa67c
Alex Kinyua <alexkinyua001alx@gmail.com>
2023-03-29 23:48:02 -0700

    reversing an array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,13 +7,13 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i , swap;
+	int i, swap;
 
 	for (i = 0; i < (n / 2); i++)
 	{
 		swap = a[i];
-		a[i] = a[n-i-1];
-		a[n-i-1] = swap;
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = swap;
 
 	}
 
