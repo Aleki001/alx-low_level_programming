f0ba732371b6ecae9f6a3c227a326b5080d03693
Alex Kinyua <alexkinyua001alx@gmail.com>
2023-03-30 09:38:57 -0700

    to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,16 +1,20 @@
-#include <stdio.h>
-#include <string.h>
+#include "main.h"
 /**
- *string_toupper - converts lowercase to upper
- *@s: parameter
- *Return: character
+ * string_toupper - change lowercase to uppercase
+ * @s:string
+ * Return:char
  */
 char *string_toupper(char *s)
 {
-	for (int i = 0; s[i] != '\0'; i++)
+
+	int i;
+
+i = 0;
+	while (*(s + i))
 	{
-		if (s[i] >= 'a' && s[i] <= 'z')
-			s[i] -= 'a' - 'A';
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+			*(s + i) -= 'a' - 'A';
+		i++;
 	}
 	return (s);
 }
