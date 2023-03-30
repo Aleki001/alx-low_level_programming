8c9f8c937f4cd02a14813e567c4038f917b06ae6
Alex Kinyua <alexkinyua001alx@gmail.com>
2023-03-30 09:33:47 -0700

    To uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 /**
- *
+ *string_toupper - converts lowercase to upper
+ *@str: parameter
+ *Return: character
+ */
+char *string_toupper(char *str)
+{
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 'a' - 'A';
+	}
+	return (str);
+}
