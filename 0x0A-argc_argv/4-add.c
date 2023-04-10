302d6646ab45d3e20ae09d3d2e273ed8fec19e36
Alex Kinyua <alexkinyua001alx@gmail.com>
2023-04-10 14:43:31 -0700

    adds the parameter

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - adds up the arguments
+ *@argc: no of arguments
+ *@argv: arguments passed
+ *Return: (0)
+ */
+int main(int argc, char *argv)
+{
+	int i, x,
+	int total = 0;
+	char *symb;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+
+		return (0);
+	}
+
+	for (i = 1; argv[i]; i++)
+	{
+		x = strtol(argv[i], &symb, 10);
+		if (*symb)
+		{
+			printf("Error\n");
+
+			return (1);
+		}
+		else
+		{
+			total += x;
+		}
+	}
+	printf("%d\n", total);
+
+	return (0);
+}
