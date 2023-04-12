815a6c2c16c50bc0ca7fb09cf9220e8691ddf281
Alex Kinyua <alexkinyua001alx@gmail.com>
2023-04-12 04:26:49 -0700

    concatenates 2 strings

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -13,7 +13,7 @@ char *argstostr(int ac, char **av)
 	int i, j;
 	char *concat, *m;
 
-	if ((sc == 0) || (av == NULL))
+	if ((ac == 0) || (av == NULL))
 		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
