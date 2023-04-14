ba9b0c7419c42eb8aea64604af5b199ab4c4fc10
Alex Kinyua <alexkinyua001alx@gmail.com>
2023-04-14 07:28:15 -0700

    reallocates memory

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,8 +11,6 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *new_ptr;
-	int i;
-	char *old_ptr = ptr;
 
 	if (new_size == old_size)
 		return (ptr);
@@ -26,17 +24,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	new_ptr = malloc(new_size);
 
-	if (new_ptr == NULL)
-		return (NULL);
-	if (new_size > old_size)
-	{
-		new_size = old_size;
-	}
-
-	for (i = 0; i < new_size; i++)
-	{
-		new_ptr[i] = old_ptr[i];
-	}
 	free(ptr);
 
 	return (new_ptr);
