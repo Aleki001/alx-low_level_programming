7702585d375e4cfcbfbbe8608203cb937125087b
Alex Kinyua <alexkinyua001alx@gmail.com>
2023-04-18 01:05:09 -0700

    created struct dog.h and added a readme file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - dog structure
+ *@name: dog's name
+ *@owner: owner of the dog
+ *@age: age of the dog
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+
+
+
+
+
+#endif
