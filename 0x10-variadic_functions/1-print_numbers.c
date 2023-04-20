342bee7042d1b7045ce05e0c64dfae9c9069c292
Alex Kinyua <alexkinyua001alx@gmail.com>
2023-04-20 03:51:43 -0700

    prints numbers and separates them with comma

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,4 +1,35 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include "variadic_functions.h"
 /**
- *print_numbers
+ *print_numbers - prints numbers
+ *@separator: separates numbers
+ *@n: no of parameters/arguments
+ *@...: infinite arguments
+ *Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+
+	unsigned int i;
+	int num;
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(list, int);
+
+		printf("%d", num);
+
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+
+	putchar('\n');
+
+	va_end(list);
+}
