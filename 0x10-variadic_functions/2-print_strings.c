68f07b46e0ba7913faa85353128925115cd34dce
Alex Kinyua <alexkinyua001alx@gmail.com>
2023-04-20 04:41:44 -0700

    prints a string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		string = va_arg(list, char *);
 
 		if (string == NULL)
-			printf("(nil)"\n");
+			printf("(nil)\n");
 		else
 			printf("%s", string);
 
