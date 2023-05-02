aebab5cc59616e9d66a9330fc3656772f992925b
Alex Kinyua <alexkinyua001alx@gmail.com>
2023-05-02 08:25:42 -0700

    free memory

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *free_listint - frees memory allocated to a list;
+ *@head: header pointer
+ *Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *node;
+
+	do {
+		node = head;
+		head = head->next;
+		free(node);
+	} while (head != 0);
+
+}
