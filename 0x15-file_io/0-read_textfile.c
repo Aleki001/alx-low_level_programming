0d4b6f4a2e33cd503652d47f31ca1dcf505796b0
Alex Kinyua <alexkinyua001alx@gmail.com>
2023-05-09 01:00:26 -0700

    read and write to posix standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ *read_textfile - reads text file and fprints tp standard output
+ *@filename: name of the file
+ *@letters: no letters it should print
+ *Return: actual no of letters it could print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	int fd;
+	ssize_t br, bw;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, 0_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(letters);
+	if (buf == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	br = read(fd, buf, letters);
+	if (br == -1)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
+
+	bw = write(STDOUT_FILENO, buf, br);
+	if (bw == -1)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
+
+	close(fd);
+	free(buf);
+
+	return (bw);
+}
