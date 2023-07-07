f19cb0523fc6b371f2e26802304472e61072a9f6
Alex Kinyua <alexkinyua001alx@gmail.com>
2023-07-06 19:55:51 -0700

    converts a binary number to an unsigned int.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,16 +11,25 @@ unsigned int binary_to_uint(const char *b)
 	int i;
 
 	if (b == NULL)
+	{
 		return (0);
+	}
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
+		{
 			return (0);
-
+		}
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
 		x <<= 1;
+
 		if (b[i] == '1')
-		       x |= 1;
+		{
+			x |= 1;
+		}
 	}
 
 	return (x);
