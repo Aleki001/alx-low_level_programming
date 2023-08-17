686c119032bbb57141b243b6a20a51961a6ccf8b
Alex Kinyua <alexkinyua001alx@gmail.com>
2023-08-17 13:31:09 -0700

    prints all elements in alist

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_dlistint - print all the elements of a list
+ * @h:    points head
+ * Return: no of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes;
+
+	for (nodes; h !=NULL; nodes++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (nodes);
+}
